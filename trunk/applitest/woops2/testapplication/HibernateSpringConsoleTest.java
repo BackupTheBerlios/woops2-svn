34a63f426d6059b856e3842298ced16599a4dbd5
garwind111 <garwind111@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2006-10-25 15:18:15 +0000

    Nouveau dossier pour les application de test Reconfiguration de Log4j (Hibernate -> Debug & Spring -> Debug)

--- trunk/applitest/woops2/testapplication/HibernateSpringConsoleTest.java
+++ trunk/applitest/woops2/testapplication/HibernateSpringConsoleTest.java
@@ -1,4 +1,4 @@
-package woops2.test.hibernate;
+package woops2.testapplication;
 
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.FileSystemResource;
@@ -7,19 +7,27 @@ import org.springframework.core.io.Resource;
 import woops2.hibernate.activity.ActivityDao;
 import woops2.model.activity.Activity;
 
-public class ConsoleTest {
+/**
+ * Application for testing hibernate and spring configuration with small model
+ * @author garwind
+ */
+public class HibernateSpringConsoleTest {
 
 	
 	public static void main(String[] args) {
+		// Getback the application context from the spring configuration file
 		Resource res = new FileSystemResource("src/applicationContext.xml");
 		XmlBeanFactory factory = new XmlBeanFactory(res);
+		// Show what is in the factory
 		System.out.println("factory => "+factory);
-		
+		// Getback the hibernateTemplate bean
 		org.springframework.orm.hibernate3.HibernateTemplate hibTempl = (org.springframework.orm.hibernate3.HibernateTemplate) factory.getBean("hibernateTemplate");
 		System.out.println("HibTemplate => "+hibTempl);
-		
+		// Get the ActivityDao Singleton for managing Activity data
 		ActivityDao dao = (ActivityDao) factory.getBean("ActivityDao");
+		// Create empty Activity
 		Activity a = new Activity();
+		// Save it
 		dao.saveOrUpdateActivity(a);
 	}
 
