c2f0141bd78050177109317dc217f5c5e110e1e6
deder <deder@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2006-11-04 20:02:44 +0000

    Update code with coding rules (this.attribute, _parameter, etc.).

--- trunk/src/woops2/model/activity/Activity.java
+++ trunk/src/woops2/model/activity/Activity.java
@@ -1,18 +1,18 @@
-package woops2.model.activity;
 
-import woops2.model.breakdownelement.WorkBreakdownElement;
+package woops2.model.activity ;
 
+import woops2.model.breakdownelement.WorkBreakdownElement ;
 
 public class Activity extends WorkBreakdownElement {
-	
-	private java.util.Set breakDownElements = new java.util.TreeSet();
 
-	public java.util.Set getBreakDownElements() {
-		return breakDownElements;
+	private java.util.Set breakDownElements = new java.util.TreeSet() ;
+
+	public java.util.Set getBreakDownElements () {
+		return this.breakDownElements ;
 	}
 
-	public void setBreakDownElements(java.util.Set breakDownElements) {
-		this.breakDownElements = breakDownElements;
+	public void setBreakDownElements (java.util.Set _breakDownElements) {
+		this.breakDownElements = _breakDownElements ;
 	}
 
 }
