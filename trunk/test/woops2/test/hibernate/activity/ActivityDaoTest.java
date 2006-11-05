372951ed669378f8294937c897a464e1f7c69532
deder <deder@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2006-11-05 20:30:21 +0000

    Add the body of tests.

--- trunk/test/woops2/test/hibernate/activity/ActivityDaoTest.java
+++ trunk/test/woops2/test/hibernate/activity/ActivityDaoTest.java
@@ -1,7 +1,17 @@
 
 package woops2.test.hibernate.activity ;
 
+import java.util.List;
+
 import junit.framework.TestCase ;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory ;
+import org.springframework.core.io.FileSystemResource ;
+import org.springframework.core.io.Resource ;
+import org.springframework.orm.hibernate3.HibernateTemplate ;
+
+import woops2.hibernate.activity.ActivityDao ;
+import woops2.model.activity.Activity ;
 
 /**
  * @author Mathieu BENOIT.
@@ -9,19 +19,81 @@ import junit.framework.TestCase ;
  */
 public class ActivityDaoTest extends TestCase {
 
+	/*
+	 * //Getback the application context from the spring configuration file Resource res = new
+	 * FileSystemResource("src/applicationContext.xml"); XmlBeanFactory factory = new
+	 * XmlBeanFactory(res); // Show what is in the factory System.out.println("factory =>
+	 * "+factory); // Getback the hibernateTemplate bean
+	 * org.springframework.orm.hibernate3.HibernateTemplate hibTempl =
+	 * (org.springframework.orm.hibernate3.HibernateTemplate) factory.getBean("hibernateTemplate");
+	 * System.out.println("HibTemplate => "+hibTempl); // Get the ActivityDao Singleton for managing
+	 * Activity data ActivityDao dao = (ActivityDao) factory.getBean("ActivityDao"); // Create empty
+	 * Activity Activity a = new Activity(); // Save it dao.saveOrUpdateActivity(a);
+	 */
+
 	/**
 	 * Test method for
 	 * {@link woops2.hibernate.activity.ActivityDao#saveOrUpdateActivity(woops2.model.activity.Activity)}.
+	 * 
+	 * PRINCIPLE
+	 * Create a tmp activity, save it into the database with the method to test.
+	 * Then look for the database to check if this tmp activity exists.
+	 * To finish delete this tmp activity from the database.
 	 */
 	public void testSaveOrUpdateActivity () {
-		fail("Not yet implemented") ; // TODO
+		// Getback the application context from the spring configuration file
+		Resource resource = new FileSystemResource("src/applicationContext.xml") ;
+		XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(resource) ;
+		// Getback the hibernateTemplate bean
+		HibernateTemplate hibernateTemplate = (HibernateTemplate) xmlBeanFactory.getBean("hibernateTemplate") ;
+		// Get the ActivityDao Singleton for managing Activity data
+		ActivityDao activityDao = (ActivityDao) xmlBeanFactory.getBean("ActivityDao") ;
+
+		// Create empty Activity
+		Activity activity = new Activity("666", "ThisPrefix") ;
+		// Save it
+		activityDao.saveOrUpdateActivity(activity) ;//FIXME the bug with the activitydao.
+		
+		// Look if this activity is also into the database.
+		Activity activityTmp = (Activity) hibernateTemplate.get(Activity.class, activity.getId()) ;
+		assertNotNull(activityTmp) ;
+		assertEquals(activity.getId(), activityTmp.getId()) ;
+		assertEquals(activity.getPrefix(), activityTmp.getPrefix()) ;
+		
+		//Delete the tmp activity from the database.
+		hibernateTemplate.delete(activityTmp);
 	}
 
 	/**
 	 * Test method for {@link woops2.hibernate.activity.ActivityDao#getAllActivities()}.
+	 * 
+	 * PRINCIPLE
+	 * Create a tmp activity, save it into the database.
+	 * Then get all activities from the database with the method to test,
+	 * and look if the size of the activities set got is >= 1.
+	 * To finish delete this tmp activity from the database.
 	 */
 	public void testGetAllActivities () {
-		fail("Not yet implemented") ; // TODO
+		//Getback the application context from the spring configuration file
+		Resource resource = new FileSystemResource("src/applicationContext.xml") ;
+		XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(resource) ;
+		// Getback the hibernateTemplate bean
+		HibernateTemplate hibernateTemplate = (HibernateTemplate) xmlBeanFactory.getBean("hibernateTemplate") ;
+		// Get the ActivityDao Singleton for managing Activity data
+		ActivityDao activityDao = (ActivityDao) xmlBeanFactory.getBean("ActivityDao") ;
+
+		// Create empty Activity
+		Activity activity = new Activity("666", "ThisPrefix") ;
+		// Save it
+		hibernateTemplate.saveOrUpdate(activity) ;
+		
+		// Look if this activity is also into the database and look if the size of the set is >= 1.
+		List <Activity> activities = activityDao.getAllActivities(); //FIXME the bug with the activitydao.
+		assertNotNull(activities) ;
+		assertTrue(activities.size() >= 1) ;
+		
+		//Delete the tmp activity from the database.
+		hibernateTemplate.delete(activity);
 	}
 
 }
