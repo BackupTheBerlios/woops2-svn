9647576e48424dcf8810cabf67157a8b3a864970
eperico <eperico@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2007-03-04 16:20:50 +0000

    [ada] - suppression des adb et ads

--- str/trunk/src_ada/package_busnetwork.ads
+++
@@ -1,9 +0,0 @@
-with package_types, package_busstop, Text_io, Ada.Exceptions, Ada.Calendar, interfaces.C;
-use package_types, package_busstop, Text_io, Ada.Exceptions, Ada.Calendar, interfaces.C;
-
-package package_busnetwork is
-
-	procedure p_initBusStop(nb_busstop : in int);
-	pragma export(CPP, p_initBusStop, "p_initBusStop");
-	
-end package_busnetwork;
