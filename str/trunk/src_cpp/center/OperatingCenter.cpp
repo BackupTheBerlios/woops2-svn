f1fc2288923d04fbef711c91cedf677f73da1611
soosuske <soosuske@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2007-03-06 14:54:16 +0000

    Source avec la prise en compte de lechange de structure

---
+++ str/trunk/src_cpp/center/OperatingCenter.cpp
@@ -0,0 +1,120 @@
+/* 
+ * File:   OperatingCenter.h
+ * Author: soosuske
+ *
+ */
+
+#include "OperatingCenter.h"
+
+
+
+
+void initSystem()
+{
+    cout<<"hello"<<endl;
+}
+
+OperatingCenter::OperatingCenter()
+{
+       cout<<"hello"<<endl;
+}
+
+OperatingCenter::~OperatingCenter()
+{
+}
+
+
+/**fonction de thread pour l'initialisation des Bus
+*@Param : 
+*/
+ void* OperatingCenter::thread_function_initBus(void *a){
+
+	//appel de la fonction ADA
+	//TODO lors de la mise en place de l'ADA	
+	//initBus();
+	cout<<"Appel a la fonction d'initialisation des bus \n"<<endl;
+	
+}
+
+/**fonction de thread pour l'écoute de la réception de bus.
+*@Param : 
+*/
+void* OperatingCenter::thread_function_receive_position(void *a){
+
+	cout<<"Appel des fonctions pour le traitement de la position"<<endl;
+	t_position* maposition = (t_position *)a;
+        cout<<"valeur :"<<maposition->busStopId<<endl;
+	
+}
+
+/**fonction de thread pour l'écoute de la réception d'information.
+*@Param : 
+*/
+void* OperatingCenter::thread_function_receive_information(void *a){
+
+	cout<<"Appel des fonctions pour le traitement de l'information"<<endl;
+	
+	
+}
+
+void OperatingCenter::initializeSystem(){
+//déclaration
+	int etat;
+	pthread_t bus_thread;
+	
+	cout<<"hello";
+	//Création des différents thread.
+	// Mise en place du Thread pour le bus
+	etat = pthread_create(&bus_thread,NULL,thread_function_initBus, NULL);
+	if (etat != 0) cout<<"Echec creation de thread pour l'initialisation des bus: %d"<<endl;
+}
+
+/**
+Méthode qui permet de recevoir la position d'un bus
+*/
+void OperatingCenter::receivePosition(t_position* position){
+    
+    
+	int etat;
+        string lol;
+        
+        //création de la structure test de la position
+        t_position* maposition = new t_position;
+        
+        //p_sendPosition(&maposition);
+        
+	//création du thread pour traiter la position du BUS
+	pthread_t receive_position_thread;
+
+	// creation du mutex
+    	//etat = pthread_mutex_init (&mutex_position, NULL);	
+    	//if (etat != 0) perror("Erreur lors de l'initialisation du mutex pour la position: %d\n");
+
+	
+	//création du thread
+	etat = pthread_create(&receive_position_thread,NULL,thread_function_receive_position, (void *)maposition);
+	if (etat != 0) perror("Echec creation de thread pour la réception des positions: %d\n");
+
+	//pthread_mutex_destroy(&mutex_position);
+    
+}
+
+
+/**
+Méthode qui permet de recevoir les informations sur le busStop
+*/
+void OperatingCenter::receiveInformation(t_information* t_ptr_t_information){
+    
+    
+	int etat;
+
+	//création du thread pour traiter l'information
+	pthread_t receive_information_thread;
+
+	//création du thread
+	etat = pthread_create(&receive_information_thread,NULL,thread_function_receive_information, NULL);
+	if (etat != 0) perror("Echec creation de thread pour la réception des positions: %d\n");
+    
+}
+
+//void OperatingCenter::p_sendPriorityMessage
---
+++ str/trunk/src_cpp/center/OperatingCenter.h
@@ -0,0 +1,41 @@
+/* 
+ * File:   OperationCenter.h
+ * Author: soosuske
+ *
+ * Created on 28 février 2007, 01:32
+ */
+
+
+
+#ifndef _OperatingCenter_H
+#define _OperatingCenter_H
+
+#include <iostream>
+#include <pthread.h>
+#include <fcntl.h>
+#include <sstream>
+#include <fstream>
+#include "Types.h"
+
+
+using namespace std;
+
+class OperatingCenter
+{
+	private :
+               static  void* thread_function_initBus(void *a);
+            	static void* thread_function_receive_position(void *a);
+            	static void* thread_function_receive_information(void *a);
+	public :
+		OperatingCenter();
+		~OperatingCenter();
+               
+		void initializeSystem();
+		void receivePosition(t_position* position);
+                void p_sendPriorityMessage();
+                void receiveInformation(t_information* t_ptr_t_information);
+                
+};
+
+#endif	/* _OperationCenter_H */
+
