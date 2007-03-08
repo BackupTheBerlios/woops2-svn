ff52f71c504350ebc24cc80b27cdf262bcea82da
garwind111 <garwind111@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2007-03-08 07:57:20 +0000

    [Java] - Main

--- str/trunk/src_java/src/Main.java
+++ str/trunk/src_java/src/Main.java
@@ -1,8 +1,13 @@
+import gui.MainFrame;
+import network.NetworkManager;
+
 
 public class Main {
 	
 	public static void main(String args[]){
-		
+		MainFrame.startGui();
+		NetworkManager nm = new NetworkManager();
+		nm.sendMessage("test");
 	}
 
 }
