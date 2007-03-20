d3c4b465cd70fea1f63d2d2a0a36394cd4533ece
soosuske <soosuske@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2007-03-20 14:35:17 +0000

    Mise a jour du centre en c++

--- str/trunk/src_ada/package_bus.ads
+++ str/trunk/src_ada/package_bus.ads
@@ -23,7 +23,7 @@ package package_bus is
     protected Radio is
         procedure sendPositionToCenter(ptr_pos : in t_ptr_t_position; speed : in int; busId : in int);
         --procedure sendPriorityMessage(ptr_mes : out t_ptr_t_priorityMessage);
-        --procedure receiveCommand(ptr_com : in t_ptr_t_action);
+        -- procedure receiveCommand(ptr_com : in t_ptr_t_action);
     end Radio;
     
 end package_bus;
