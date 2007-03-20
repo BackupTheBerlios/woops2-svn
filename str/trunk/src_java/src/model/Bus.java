86dd4400883722f0f3527f0e1da8ef46bcdaa4d0
themorpheus <themorpheus@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2007-03-20 11:38:41 +0000

    ajout du boolen isRunning pour le bus

--- str/trunk/src_java/src/model/Bus.java
+++ str/trunk/src_java/src/model/Bus.java
@@ -10,6 +10,8 @@ public class Bus extends SystemObject {
 	
 	private BusEntity representation;
 	
+	private boolean isRunning;
+	
 	/**
 	 * 
 	 * @param _id
@@ -18,6 +20,7 @@ public class Bus extends SystemObject {
 		this.id = _id;
 		this.line = _l;
 		this.representation = new BusEntity("resources/images/dot_b.png", _x, _y);
+		this.isRunning = false;
 	}
 
 	/**
@@ -60,6 +63,20 @@ public class Bus extends SystemObject {
 	 */
 	public void setRepresentation(BusEntity representation) {
 		this.representation = representation;
+	}
+
+	/**
+	 * @return the isRunning
+	 */
+	public boolean isRunning() {
+		return isRunning;
+	}
+
+	/**
+	 * @param isRunning the isRunning to set
+	 */
+	public void setRunning(boolean isRunning) {
+		this.isRunning = isRunning;
 	}
 	
 }
