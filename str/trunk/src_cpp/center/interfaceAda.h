32053a34740079cd57e5af59cf63b63435664fdc
deder <deder@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2007-03-20 15:31:04 +0000

    [Cpp] ajout des fonctions start, stop, accelerate et decelerate.

--- str/trunk/src_cpp/center/interfaceAda.h
+++ str/trunk/src_cpp/center/interfaceAda.h
@@ -3,5 +3,9 @@
 
 void adainit_busStop(int nombre, int ligne);
 void adainit_bus(int nombre, int ligne);
-void ada_startBus(int id);
+void adastart_bus(int busId, int lineId);
+void adastop_bus(int busId, int lineId);
+void adaaccelerate_bus(int busId, int lineId);
+void adadecelerate_bus(int busId, int lineId);
+
 #endif	/* _OperationCenter_H */
