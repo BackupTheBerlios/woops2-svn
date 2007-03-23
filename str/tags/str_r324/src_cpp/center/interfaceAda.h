f6391ac3de63a229fe12253e360e3331ec788b06
garwind111 <garwind111@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2007-03-23 00:25:16 +0000

    supp d'un tag

--- str/tags/str_r324/src_cpp/center/interfaceAda.h
+++
@@ -1,7 +0,0 @@
-#ifndef _interfaceAda_H
-#define _interfaceAda_H
-
-void adainit_busStop(int nombre, int ligne);
-void adainit_bus(int nombre, int ligne);
-
-#endif	/* _OperationCenter_H */
--- str/tags/str_r324/src_java/src/gui/sprite/entities/BusStopEntity.java
+++
@@ -1,5 +0,0 @@
-package gui.sprite.entities;
-
-public class BusStopEntity {
-
-}
