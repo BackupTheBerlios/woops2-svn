ceeca9ed83cb62b9c93ae7de4041c9808785a49c
garwind111 <garwind111@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2008-03-25 17:19:10 +0000

    modifs services & implementation services action

--- MarketProject/trunk/src/isimarket/db/Test.java
+++ MarketProject/trunk/src/isimarket/db/Test.java
@@ -17,7 +17,9 @@ public class Test {
 		Statement s = null;
 		try {
 			s = dbman.getConnection().createStatement();
-			s.execute("create table Test()");
+			
+			s.execute("create table ActionType(code varchar(2),label varchar(2),introductionDate varchar(10) ,introductionPrice float , quantity integer , currentPrice float )");
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
