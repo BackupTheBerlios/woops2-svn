6dbe95b2471c07b444d1fa9c8c18ca0189bf3a5d
garwind111 <garwind111@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2008-03-29 10:12:18 +0000

    maj TableManager

--- MarketProject/trunk/src/isimarket/db/manager/TableManager.java
+++ MarketProject/trunk/src/isimarket/db/manager/TableManager.java
@@ -10,25 +10,25 @@ public class TableManager {
 	private static final String _DROP_ALARMTYPE = "drop table alarmtype";
 	
 	// ActionType
-	private static final String _CREATE_ACTIONTYPE = "create table actiontype(code varchar(2),"
-			+ "label varchar(3),introduction_date varchar(10) ,introduction_price float , "
+	private static final String _CREATE_ACTIONTYPE = "create table actiontype(code varchar(5),"
+			+ "label varchar(32),introduction_date varchar(10) ,introduction_price float , "
 			+ "quantity integer , current_price float, primary key (code) )";
 	private static final String _DROP_ACTIONTYPE = "drop table actiontype";
 
 	// Action
 	private static final String _CREATE_ACTION = "create table action(action_id integer, buy_price float, buy_date varchar(10),"
-			+ " quantity integer, action_type_code varchar(2), wallet_id integer, primary key (action_id),"
+			+ " quantity integer, action_type_code varchar(5), wallet_id integer, primary key (action_id),"
 			+ " foreign key (action_type_code) references actiontype(code),"
 			+ " foreign key (wallet_id) references wallet(wallet_id))";
 	private static final String _DROP_ACTION = "drop table action";
 
 	// Event
-	private static final String _CREATE_EVENT = "create table event(event_id integer, price float, date varchar(10),action_type_code varchar(2),"
+	private static final String _CREATE_EVENT = "create table event(event_id integer, price float, date varchar(10),action_type_code varchar(5),"
 			+ " primary key (event_id), foreign key (action_type_code) references actiontype(code))";
 	private static final String _DROP_EVENT = "drop table event";
 
 	// Alarm
-	private static final String _CREATE_ALARM = "create table alarm(alarm_id integer, name varchar(32), alarm_type_id integer ,action_type_code varchar(2),"
+	private static final String _CREATE_ALARM = "create table alarm(alarm_id integer, name varchar(32), alarm_type_id integer ,action_type_code varchar(5),"
 			+ " wallet_id integer, primary key (alarm_id), "
 			+ " foreign key (wallet_id) references wallet(wallet_id),"
 			+ " foreign key (alarm_type_id) references alarmtype(alarm_type_id),"
@@ -51,32 +51,44 @@ public class TableManager {
 	private static final String _DROP_ADMINISTRATOR = "drop table administrator";
 	
 	public static void main(String[] args) {
+		dropAndCreateTables();
+	}
+
+	private static void dropAndCreateTables() {
 		DatabaseManager dbman = DatabaseManager.getInstance();
 		Statement s = null;
 		try {
 			s = dbman.getConnection().createStatement();
-//			s.executeUpdate(_DROP_ALARM);
-//			s.executeUpdate(_DROP_ACTION);
-//			s.executeUpdate(_DROP_EVENT);
-//			s.executeUpdate(_DROP_OPERATOR);
-//			s.executeUpdate(_DROP_WALLET);
-//			s.executeUpdate(_DROP_ALARMTYPE);
-//			s.executeUpdate(_DROP_ACTIONTYPE);
-//			s.executeUpdate(_DROP_ADMINISTRATOR);
 			
-			s.executeUpdate(_CREATE_ACTIONTYPE);
-			s.executeUpdate(_CREATE_ALARMTYPE);
-			s.executeUpdate(_CREATE_ADMINISTRATOR);
-			s.executeUpdate(_CREATE_WALLET);
-			s.executeUpdate(_CREATE_EVENT);
-			s.executeUpdate(_CREATE_ACTION);
-			s.executeUpdate(_CREATE_ALARM);
-			s.executeUpdate(_CREATE_OPERATOR);
+			dropTables(s);
+			createTables(s);
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			dbman.closeConnection();
 		}
+	}
+
+	private static void dropTables(Statement s) throws SQLException {
+		s.executeUpdate(_DROP_ALARM);
+		s.executeUpdate(_DROP_ACTION);
+		s.executeUpdate(_DROP_EVENT);
+		s.executeUpdate(_DROP_OPERATOR);
+		s.executeUpdate(_DROP_WALLET);
+		s.executeUpdate(_DROP_ALARMTYPE);
+		s.executeUpdate(_DROP_ACTIONTYPE);
+		s.executeUpdate(_DROP_ADMINISTRATOR);
+	}
+
+	private static void createTables(Statement s) throws SQLException {
+		s.executeUpdate(_CREATE_ACTIONTYPE);
+		s.executeUpdate(_CREATE_ALARMTYPE);
+		s.executeUpdate(_CREATE_ADMINISTRATOR);
+		s.executeUpdate(_CREATE_WALLET);
+		s.executeUpdate(_CREATE_EVENT);
+		s.executeUpdate(_CREATE_ACTION);
+		s.executeUpdate(_CREATE_ALARM);
+		s.executeUpdate(_CREATE_OPERATOR);
 	}
 }
