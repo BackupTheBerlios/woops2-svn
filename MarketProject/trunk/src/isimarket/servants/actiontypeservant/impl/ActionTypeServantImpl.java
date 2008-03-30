473e94f32f762bcd49aef6e56e4facb6a52301df
garwind111 <garwind111@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2008-03-30 14:56:26 +0000

    servant actiontype

---
+++ MarketProject/trunk/src/isimarket/servants/actiontypeservant/impl/ActionTypeServantImpl.java
@@ -0,0 +1,34 @@
+package isimarket.servants.actiontypeservant.impl;
+
+import isimarket.model.ActionType;
+import isimarket.model.Event;
+import isimarket.servants.actiontypeservant._ActionTypeServantImplBase;
+
+public class ActionTypeServantImpl extends _ActionTypeServantImplBase{
+
+	public void createNewActionType(ActionType _newActionType) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ActionType getActionType(String _actionTypeCode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ActionType[] getActionTypeList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Event[] getEvents(String _actionTypeCode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateActionType(ActionType _newActionType) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
