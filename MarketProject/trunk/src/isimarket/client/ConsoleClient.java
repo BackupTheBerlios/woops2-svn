14b559f1775713de5577aea04721bbd1e124d53d
garwind111 <garwind111@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2008-04-08 16:24:59 +0000

    correction client & affichages dans admin servant

--- MarketProject/trunk/src/isimarket/client/ConsoleClient.java
+++ MarketProject/trunk/src/isimarket/client/ConsoleClient.java
@@ -1,16 +1,22 @@
 package isimarket.client;
-import java.util.Properties;
-
-import isimarket.model.Operator;
-import isimarket.model.Wallet;
+import isimarket.servants.actiontypeservant.ActionTypeServant;
+import isimarket.servants.actiontypeservant.ActionTypeServantHelper;
 import isimarket.servants.administrationservant.AdministrationServant;
 import isimarket.servants.administrationservant.AdministrationServantHelper;
 import isimarket.server.ServerConstants;
 
+import java.util.Properties;
+
 import org.omg.CORBA.ORB;
+import org.omg.CORBA.Object;
 import org.omg.CosNaming.NameComponent;
 import org.omg.CosNaming.NamingContext;
 import org.omg.CosNaming.NamingContextHelper;
+import org.omg.CosNaming.NamingContextPackage.CannotProceed;
+import org.omg.CosNaming.NamingContextPackage.InvalidName;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+
+import com.sun.corba.se.spi.orbutil.fsm.Action;
 
 
 public class ConsoleClient {
@@ -29,17 +35,29 @@ public class ConsoleClient {
 			NamingContext ncRef = NamingContextHelper.narrow(objRef);
 
 			// resolve the Object Reference in Naming
-			NameComponent nc = new NameComponent(ServerConstants._REF_ADMINISTRATION_SERVANT, "");
-			NameComponent path[] = {nc};
-			AdministrationServant administrationServantRef = AdministrationServantHelper.narrow(ncRef.resolve(path));
-			System.out.println("administrationServantRef connected @ "+administrationServantRef);
+//			NameComponent nc = new NameComponent(ServerConstants._REF_ADMINISTRATION_SERVANT, "");
+//			NameComponent path[] = {nc};
+//			AdministrationServant administrationServantRef = AdministrationServantHelper.narrow(ncRef.resolve(path));
 			
+			AdministrationServant administrationServantRef = AdministrationServantHelper.narrow((Object) bindReference(ncRef,ServerConstants._REF_ADMINISTRATION_SERVANT ));
+			ActionTypeServant actionTypeServantRef = ActionTypeServantHelper.narrow((Object) bindReference(ncRef,ServerConstants._REF_ACTION_SERVANT ));
+			
+			System.out.println("administrationServantRef @ "+administrationServantRef);
+			System.out.println("actionTypeServantRef @"+actionTypeServantRef);
+			// TODO creer un operator test
 			//administrationServantRef.createOperator("test","password",1000.0f);
-			administrationServantRef.updateCash("test",2000.0f);
+			administrationServantRef.updateCash("test",3000.0f);
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private static Object bindReference(NamingContext ncRef, String name) 
+			throws NotFound, CannotProceed, InvalidName{
+		NameComponent nc = new NameComponent(name, "");
+		NameComponent path[] = {nc};
+		return ncRef.resolve(path);
 	}
 
 }
--- MarketProject/trunk/src/isimarket/servants/administrationservant/impl/AdministrationServantImpl.java
+++ MarketProject/trunk/src/isimarket/servants/administrationservant/impl/AdministrationServantImpl.java
@@ -15,20 +15,30 @@ public class AdministrationServantImpl extends _AdministrationServantImplBase {
 	protected WalletDao walletDao = new WalletDao();
 
 	public void createWallet(float _initalCash) {
+		System.out.println("createWallet : "+_initalCash);
+		
 		Wallet wallet = new Wallet();
 		wallet.cash = _initalCash;
 		this.walletDao.insert(wallet);
+		
+		System.out.println("createWallet ok");
 	}
 
 	public void updateCash(String _operatorLogin, float _newCash) {
+		System.out.println("updateCash : "+_operatorLogin+" "+_newCash);
+		
 		Operator operator = this.operatorDao.get(_operatorLogin);
 		Wallet wallet = operator.wallet;
 		wallet.cash = _newCash;
 		this.walletDao.updateCash(wallet);
+		
+		System.out.println("updateCash ok");
 	}
 
 	public void createOperator(String _operatorLogin, String _password,
 			float inital_Cash) {
+		System.out.println("createOperator : "+_operatorLogin+" "+_password+" "+inital_Cash);
+		
 		this.createWallet(inital_Cash);
 		Wallet wallet = this.walletDao.getLastWallet();
 		Operator op = new Operator();
@@ -37,6 +47,7 @@ public class AdministrationServantImpl extends _AdministrationServantImplBase {
 		op.password = _password;
 		this.operatorDao.insert(op);
 		
+		System.out.println("createOperator ok");
 	}
 
 }
