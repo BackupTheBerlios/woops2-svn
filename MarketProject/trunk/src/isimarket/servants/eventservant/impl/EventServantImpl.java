72510bbd3d21b0664b4f8fbb44560e291b9b7fee
themorpheus <themorpheus@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2008-04-21 17:09:11 +0000

    implementation de l'eventservant

--- MarketProject/trunk/src/isimarket/servants/eventservant/impl/EventServantImpl.java
+++ MarketProject/trunk/src/isimarket/servants/eventservant/impl/EventServantImpl.java
@@ -1,34 +1,49 @@
 package isimarket.servants.eventservant.impl;
 
+import java.util.List;
+
+import isimarket.db.dao.ActionTypeDao;
+import isimarket.db.dao.EventDao;
+import isimarket.model.ActionType;
 import isimarket.model.Event;
 import isimarket.servants.eventservant._EventServantImplBase;
+import isimarket.server.ServerConstants;
 
 public class EventServantImpl extends _EventServantImplBase {
 
 	private static final long serialVersionUID = 1L;
+	
+	protected EventDao eventDao = new EventDao();
+	
+	protected ActionTypeDao actionTypeDao = new ActionTypeDao();
 
 	@Override
 	public void createEvent(String date, float price, String actionTypeCode) {
-		// TODO Auto-generated method stub
-
+		ActionType actionType = this.actionTypeDao.get(actionTypeCode);		
+		
+		Event event = new Event();
+		event.date = ServerConstants.now();
+		event.price = price;
+		event.actiontype = actionType;
+		
+		this.eventDao.insert(event);
 	}
 
 	@Override
 	public void deleteEvent(int eventId) {
-		// TODO Auto-generated method stub
-
+		this.eventDao.delete(eventId);
 	}
 
 	@Override
 	public Event get(int eventId) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.eventDao.get(eventId);
 	}
 
 	@Override
 	public Event[] getEventListFromActionType(String actionTypeCode) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Event> eventlist = this.eventDao.getAllFromActionType(actionTypeCode);
+		Event[] eventArray = new Event[eventlist.size()];
+		return eventlist.toArray(eventArray);
 	}
 
 }
