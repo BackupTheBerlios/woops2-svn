e4517b6f7725e6c2c97a46371f29932735f0de65
garwind111 <garwind111@3965b01a-3b1f-0410-9a36-b2b4c4ae782b>
2008-05-25 18:46:32 +0000

    v7

---
+++ IsiMarketClient/tags/trunk/src/isimarketclient/IsiMarketClientModel.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package isimarketclient;
+
+import isimarket.model.Action;
+import isimarket.model.ActionType;
+import isimarket.model.Alarm;
+import isimarket.model.AlarmType;
+import isimarket.model.Wallet;
+
+/**
+ *
+ * @author Arno
+ */
+public class IsiMarketClientModel {
+    public static String login;
+    public static String password;
+    public static Wallet wallet;
+    public static Action[] actions;
+    public static ActionType[] market;
+    public static Alarm[] alarms;
+    public static AlarmType[] alarmTypes;
+}
